185700550825685bfbd842cf59d8b89f3dad6708
Dylanchax <dylanchax@gmail.com>
2022-05-16 15:35:59 +0200

    Exercise 4

--- Lab2.php
+++ Lab2.php
@@ -4,3 +4,11 @@ function first( string $text): void
     echo ($text);
 }
 first('param3t3r');
+function repeatContent(int $count = 1):void 
+{
+    for($i=0:$i <=$count:$i++){
+        first("Hey There");
+    }
+}
+repeatContent();
+repeatContent(5);
