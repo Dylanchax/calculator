95b9ffc297691adf5ee1af5642cfaf6dd7e4040e
Dylanchax <dylanchax@gmail.com>
2022-05-25 02:15:51 +0200

    ex 8

---
+++ super-calculator1.php
@@ -0,0 +1,20 @@
+<?php
+
+function sum(float $num1,float $num2): float{
+    return $num1 + $num2;    
+}
+function diff(float $num1,float $num2): float{
+    return $num1 - $num2;    
+}
+function div(float $num1,float $num2): float{
+    return $num1 / $num2;    
+}
+function mult(float $num1,float $num2): float{
+    return $num1 * $num2;    
+}
+
+function calculator (string $userformula): float
+
+{  
+    echo ($userformula);
+   
